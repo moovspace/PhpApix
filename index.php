ab2993601afe3bec2e2cd3a6ed439dbb26a3d03a
Marcys <moovspace@github.com>
2020-01-19 13:58:57 +0100

    Php api router with composer autoload

--- index.php
+++ index.php
@@ -5,19 +5,4 @@ require("vendor/autoload.php");
 require("phpini.php");
 // Router, routes
 require('router.php');
-
-$u = '/user/{id}/blog';
-
-function getParam($id = '{id}', $url, $route){
-	$u = explode('/', $url);
-	$r = explode('/', $route);
-	foreach ($r as $k => $v) {
-		if($v == $id){
-			return $u[$k];
-		}
-	}
-	return false;
-}
-
-// echo getParam('/user/Maxiu/blog', '/user/{id}/blog', '{id}');
 ?>
