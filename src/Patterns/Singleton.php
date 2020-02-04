1f8dfc66c05cd8c47e7bbf05f1565d0a034346f2
Marcys <moovspace@github.com>
2020-02-04 16:07:18 +0100

    PhpApix router with composer

---
+++ src/Patterns/Singleton.php
@@ -0,0 +1,50 @@
+<?php 
+declare(strict_types=1);
+
+namespace PhpAPix\Patterns;
+
+final class Singleton
+{
+	// private static $instance = null;
+
+	/**
+	 * Gets the instance
+	 */
+	public static function getInstance(): self
+	{
+		static $instance;
+
+		if (null === $instance) {
+			$instance = new self();
+			// static::$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Is not allowed to call from outside to prevent from creating multiple instances,
+	 * to use the singleton, you have to obtain the instance from Singleton::getInstance() instead
+	 * protected or private
+	 */
+	protected function __construct()
+	{
+	}
+
+	/**
+	 * prevent the instance from being cloned (which would create a second instance of it)
+	 * protected or private
+	 */
+	protected function __clone()
+	{
+	}
+
+	/**
+	 * prevent from being unserialized (which would create a second instance of it)
+	 * protected or private
+	 */
+	protected function __wakeup()
+	{
+	}
+}
+?>
---
+++ src/Patterns/Tests/SingletonTest.php
@@ -0,0 +1,20 @@
+<?php 
+declare(strict_types=1);
+
+namespace Patterns\Tests;
+
+use Patterns\Singleton;
+use PHPUnit\Framework\TestCase;
+
+class SingletonTest extends TestCase
+{
+    public function testUniqueness()
+    {
+        $firstCall = Singleton::getInstance();
+        $secondCall = Singleton::getInstance();
+        
+        $this->assertInstanceOf(Singleton::class, $firstCall);
+        $this->assertSame($firstCall, $secondCall);
+    }
+}
+?>
